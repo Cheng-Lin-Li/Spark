02f6e9040c29aa6a418e45ace560a65f5ffed45e
Cheng-Lin Li <clark.cl.li@gmail.com>
2018-03-09 02:16:08 -0800

    minor modify on variable

--- A-Priori_SON/A-Priori_SON.py
+++ A-Priori_SON/A-Priori_SON.py
@@ -10,11 +10,11 @@ Finding Frequent Itemsets: SON Algorithm by A-Priori algorithm in stage 1
 
 @license:    Licensed under the GNU v3.0. https://www.gnu.org/licenses/gpl.html
 
-@contact:    chenglil@usc.edu
+@contact:    chenglil@usc.edu, clark.cl.li@gmail.com
 @version:    1.1 Performance improvement on SON phase 1 & 2.
 
 @create:    February, 23, 2017
-@updated:   February, 27, 2017
+@updated:   March, 9, 2018
 '''
 
 from __future__ import print_function 
@@ -60,33 +60,33 @@ class A_Priori(object):
         _CTable = A_Priori.CTable
         _baskets = list(baskets)
         _mini_sup = A_Priori.mini_sup
-        _combination_itemsets = set()
-        _total_baskets = 0
-        _count_threshold = 0
-        _item_pairs = 1
-        _hasFreqItems = False
+
+        total_baskets = 0
+        count_threshold = 0
+        item_pairs = 1
+        hasFreqItems = False
         if (_baskets is None):
-            _hasFreqItems = False
+            hasFreqItems = False
             return None
         else:
-            _hasFreqItems = True
+            hasFreqItems = True
         if PRINT_TIME : print ('A_Priori.execute=>Start=>%s'%(str(datetime.now())))   
         if DEBUG : print ('A_Priori.execute=>mini_sup=%d, baskets=%s'%(_mini_sup, str(_baskets)))
         
         #Scan the buckets to generate singleton list frequent items and count table with counts
-        A_Priori.setFreqItems(_baskets, _CTable, _mini_sup, _item_pairs, _count_threshold)
-        _total_baskets = len(_baskets)
+        A_Priori.setFreqItems(_baskets, _CTable, _mini_sup, item_pairs, count_threshold)
+        total_baskets = len(_baskets)
         
-        _count_threshold = math.ceil(_total_baskets * _mini_sup)
-        while _hasFreqItems:
-            if _item_pairs > 1 : _hasFreqItems= A_Priori.getFreqItems(_baskets, _CTable, _mini_sup, _item_pairs, _count_threshold)
+        count_threshold = math.ceil(total_baskets * _mini_sup)
+        while hasFreqItems:
+            if item_pairs > 1 : hasFreqItems= A_Priori.getFreqItems(_baskets, _CTable, _mini_sup, item_pairs, count_threshold)
             # Check frequent threshold.
             for _itemp, _counts in _CTable.items():
-                if _counts >= _count_threshold:
+                if _counts >= count_threshold:
                     A_Priori.k_1_freq_itemsets.append(_itemp)
                     A_Priori.freq_itemsets.append(_itemp)
-            _item_pairs += 1
-            _CTable = A_Priori.getCountTable(A_Priori.k_1_freq_itemsets, _item_pairs) 
+            item_pairs += 1
+            _CTable = A_Priori.getCountTable(A_Priori.k_1_freq_itemsets, item_pairs) 
             A_Priori.k_1_freq_itemsets = []
         
         if DEBUG : print ('A_Priori.execute=>self.freq_itemsets=%s'%(str(A_Priori.freq_itemsets)))   
@@ -97,73 +97,74 @@ class A_Priori(object):
     @staticmethod 
     def setFreqItems(baskets, count_table, mini_sup, item_pair_no, _count_threshold):
         # First time to build up count table base on singleton itemset by scan whole baskets.
-        _count_table = count_table
+        count_table = count_table
         if PRINT_TIME : print ('A_Priori.setFreqItems=>Start=>%s'%(str(datetime.now())))
         
         for _basket in baskets:
             for _itemset in _basket:
-                _ct = _count_table.get(_itemset)
+                _ct = count_table.get(_itemset)
                 if _ct == None: #First time scan baskets, build the singleton item into count_table
                     #build the count table
-                    _count_table.update({_itemset:1})
+                    count_table.update({_itemset:1})
                 elif _ct != None:
-                    _count_table[_itemset] += 1  
+                    count_table[_itemset] += 1  
                                                                   
         if PRINT_TIME : print ('A_Priori.setFreqItems=>Finish=>%s'%(str(datetime.now())))                    
-        if DEBUG : print ('A_Priori.setFreqItems=>Time:%s, _count_table==%s'%(str(datetime.now()),str(_count_table)))              
-        return _count_table
+        if DEBUG : print ('A_Priori.setFreqItems=>Time:%s, count_table==%s'%(str(datetime.now()),str(count_table)))              
+        return count_table
+    
     @staticmethod    
     def getFreqItems(baskets, count_table, mini_sup, item_pair_no, _count_threshold):
         #Get frequent K=item_pair_no itemsets. (6)
-        _count_table = count_table
-        _hasFreqItems = False
+        count_table = count_table
+        hasFreqItems = False
         if PRINT_TIME : print ('A_Priori.getFreqItems=>Start=>%s'%(str(datetime.now())))
                 
         for _basket in baskets:
             
-            for _itemset in _count_table.keys():            
+            for _itemset in count_table.keys():            
             # Remove over threshold frequent itemsets from count table 
-                if (_count_table[_itemset] >= _count_threshold):
+                if (count_table[_itemset] >= _count_threshold):
                     A_Priori.k_1_freq_itemsets.append(_itemset)
                     A_Priori.freq_itemsets.append(_itemset)
-                    del _count_table[_itemset]                            
-            for _itemset in _count_table.keys():
+                    del count_table[_itemset]                            
+            for _itemset in count_table.keys():
                 if set(_itemset.split(SPLITTER)).issubset(_basket):
-                    _hasFreqItems = True
-                    _count_table[_itemset] += 1
+                    hasFreqItems = True
+                    count_table[_itemset] += 1
 
         if PRINT_TIME : print ('A_Priori.getFreqItems=>Finish=>%s'%(str(datetime.now())))                                
-        if DEBUG : print ('A_Priori.getFreqItems=>Time:%s, _count_table==%s'%(str(datetime.now()), str(_count_table)))              
-        return _hasFreqItems
+        if DEBUG : print ('A_Priori.getFreqItems=>Time:%s, count_table==%s'%(str(datetime.now()), str(count_table)))              
+        return hasFreqItems
 
     @staticmethod 
     def getCountTable(fre_itemsets, item_pair_no):
         # Base on k-1 pass frequent itemsets and target number of pairs to build count_table for pass k
-        _itemsets = set()
-        _tmp_itemsets = set()
-        _count_table = {}
-        _fre_itemsets = fre_itemsets
+        itemsets = set()
+        tmp_itemsets = set()
+        count_table = {}
+        fre_itemsets = fre_itemsets
         if PRINT_TIME : print ('A_Priori.getCountTable=>Start=>%s'%(str(datetime.now())))
-        if DEBUG : print ('A_Priori.getCountTable=>item_pair_no=%d, _fre_itemsets==%s'%(item_pair_no, str(_fre_itemsets)))
+        if DEBUG : print ('A_Priori.getCountTable=>item_pair_no=%d, fre_itemsets==%s'%(item_pair_no, str(fre_itemsets)))
 
         # Get each item from frequent itemsets and put into a set.
-        for _items in _fre_itemsets: 
+        for _items in fre_itemsets: 
             #if DEBUG : print ('A_Priori.getCountTable=>_items==%s'%(str(_items)))
             for _item in _items.split(SPLITTER): # Countable as baskets                   
-                _tmp_itemsets.add((_item))
+                tmp_itemsets.add((_item))
         
         #Base on previous frequent itemsets to construct count table
-        for _c in combinations(_tmp_itemsets, item_pair_no):
-            _itemsets.add(SPLITTER.join(sorted(_c, key=int)))
+        for _c in combinations(tmp_itemsets, item_pair_no):
+            itemsets.add(SPLITTER.join(sorted(_c, key=int)))
         
-        if DEBUG : print('_fre_itemsets=%s'%_fre_itemsets)
-        if DEBUG : print('_itemsets=%s'%_itemsets)    
-        for _item in _itemsets:
+        if DEBUG : print('fre_itemsets=%s'%fre_itemsets)
+        if DEBUG : print('itemsets=%s'%itemsets)    
+        for _item in itemsets:
             #if DEBUG : print ('A_Priori.getCountTable=>_item=='+str(_item))
             for _newset in combinations(_item.split(SPLITTER), item_pair_no-1): 
                 #check all new candidate itemsets should be in previous itemsets
                 _isExist = False
-                for _exist_it in _fre_itemsets:
+                for _exist_it in fre_itemsets:
                     #if DEBUG : print ('_exist_it=%s, SPLITTER.join(_newset)=%s _exist_it == _newset[0]=>%r'%(_exist_it, SPLITTER.join(_newset), _exist_it == SPLITTER.join(_newset)))
                     if _exist_it == SPLITTER.join(_newset):
                         _isExist = True
@@ -172,10 +173,10 @@ class A_Priori(object):
                 if _isExist == False:
                     break
                 else:    
-                    _count_table.update({(_item):0})
+                    count_table.update({(_item):0})
         if PRINT_TIME : print ('A_Priori.getCountTable=>Finish=>%s'%(str(datetime.now())))        
-        if DEBUG : print ('A_Priori.getCountTable=>item_pair_no=%d, _count_table==%s'%(item_pair_no, str(_count_table)))            
-        return _count_table
+        if DEBUG : print ('A_Priori.getCountTable=>item_pair_no=%d, count_table==%s'%(item_pair_no, str(count_table)))            
+        return count_table
                 
     
 class SON(object):
@@ -232,52 +233,51 @@ class SON(object):
         return self.global_freq_itemsets
            
     def Phase1(self, rddbaskets, mini_sup):
-        _baskets = baskets
-        _mini_sup = mini_sup
-        _local_freq_itemsets = None
+        mini_sup = mini_sup
+        local_freq_itemsets = None
         if PRINT_TIME : print ('SON.Phase1=>Start=>%s'%(str(datetime.now())))           
-        _rddListBaskets = rddbaskets.map(lambda line: [ _item for _item in line.strip().split(SPLITTER)]).persist()
-        if DEBUG : print ('SON.Phase1=>_rddListBaskets=%s'%(str(_rddListBaskets.collect())))
+        rddListBaskets = rddbaskets.map(lambda line: [ _item for _item in line.strip().split(SPLITTER)]).persist()
+        if DEBUG : print ('SON.Phase1=>rddListBaskets=%s'%(str(rddListBaskets.collect())))
 
-        _local_freq_itemsets = _rddListBaskets.mapPartitions(self.phase1_alg.execute).collect()        
-        #_mergeLocalFreqItems = self.sc.parallelize(_local_freq_itemsets).map(lambda _itemset: (_itemset,1)).reduceByKey(lambda _accum, _val: _accum + _val).map(lambda (_itemset, _count): _itemset).collect()
-        _mergeLocalFreqItems = self.sc.parallelize(_local_freq_itemsets).map(lambda _itemset: (_itemset,1)).reduceByKey(lambda _accum, _val: _val).collect()
-        if DEBUG : print ('SON.Phase1=>_mergeLocalFreqItems=%s'%(str(_mergeLocalFreqItems)))
+        local_freq_itemsets = rddListBaskets.mapPartitions(self.phase1_alg.execute).collect()        
+        #mergeLocalFreqItems = self.sc.parallelize(local_freq_itemsets).map(lambda _itemset: (_itemset,1)).reduceByKey(lambda _accum, _val: _accum + _val).map(lambda (_itemset, _count): _itemset).collect()
+        mergeLocalFreqItems = self.sc.parallelize(local_freq_itemsets).map(lambda _itemset: (_itemset,1)).reduceByKey(lambda _accum, _val: _val).collect()
+        if DEBUG : print ('SON.Phase1=>mergeLocalFreqItems=%s'%(str(mergeLocalFreqItems)))
         if PRINT_TIME : print ('SON.Phase1=>Finish=>%s'%(str(datetime.now())))                 
-        return _mergeLocalFreqItems      
+        return mergeLocalFreqItems    
         
     def Phase2(self, rddbaskets, mini_sup, local_freq_itemsets):
-        _global_freq_itemsets = None
-        _count_threshold = math.ceil(self.total_buskets * mini_sup)
+        global_freq_itemsets = None
+        count_threshold = math.ceil(self.total_buskets * mini_sup)
         
         if PRINT_TIME : print ('SON.Phase2=>Start=>%s'%(str(datetime.now())))
         if DEBUG : print ('SON.Phase2=>rddbaskets=>%s'%(rddbaskets.collect()))            
-        #_rddglobal_counts = rddbaskets.flatMap(lambda basket: [(_itemset[0], 1) for _itemset in local_freq_itemsets if set(str(_itemset[0]).split(SPLITTER)).issubset(basket.split(SPLITTER))])
+        #rddglobal_counts = rddbaskets.flatMap(lambda basket: [(_itemset[0], 1) for _itemset in local_freq_itemsets if set(str(_itemset[0]).split(SPLITTER)).issubset(basket.split(SPLITTER))])
 
-        _local_freq_itemsets = self.sc.broadcast(local_freq_itemsets) #broadcast the itemsets data to each worker in distribution environment.
-        # _local_freq_itemsets.value to retrieve the data list.
-        _rddglobal_counts = rddbaskets.mapPartitions(lambda basket: SON.getCount(basket, _local_freq_itemsets)).collect()
-        #_rddglobal_counts = rddbaskets.mapPartitions(lambda baskets: iter([(_itemset[0], 1) for _basket in baskets for _itemset in local_freq_itemsets.value if set(str(_itemset[0]).split(SPLITTER)).issubset(_basket.split(SPLITTER))])).collect()
+        local_freq_itemsets = self.sc.broadcast(local_freq_itemsets) #broadcast the itemsets data to each worker in distribution environment.
+        # local_freq_itemsets.value to retrieve the data list.
+        rddglobal_counts = rddbaskets.mapPartitions(lambda basket: SON.getCount(basket, local_freq_itemsets)).collect()
+        #rddglobal_counts = rddbaskets.mapPartitions(lambda baskets: iter([(_itemset[0], 1) for _basket in baskets for _itemset in local_freq_itemsets.value if set(str(_itemset[0]).split(SPLITTER)).issubset(_basket.split(SPLITTER))])).collect()
         
-        if DEBUG : print ('SON.Phase2=>_rddglobal_counts=%s'%(str(_rddglobal_counts)))
-        _global_freq_itemsets = self.sc.parallelize(_rddglobal_counts).reduceByKey(lambda _accum, _val: _accum + _val).filter(lambda (_itemset, _counts): _counts >= _count_threshold).collect()
+        if DEBUG : print ('SON.Phase2=>rddglobal_counts=%s'%(str(rddglobal_counts)))
+        global_freq_itemsets = self.sc.parallelize(rddglobal_counts).reduceByKey(lambda _accum, _val: _accum + _val).filter(lambda (_itemset, _counts): _counts >= count_threshold).collect()
                 
-        if DEBUG : print ('SON.Phase2=>_global_freq_itemsets=%s'%(str(_global_freq_itemsets)))
+        if DEBUG : print ('SON.Phase2=>global_freq_itemsets=%s'%(str(global_freq_itemsets)))
         if PRINT_TIME : print ('SON.Phase2=>Finish=>%s'%(str(datetime.now())))         
-        return _global_freq_itemsets
+        return global_freq_itemsets
          
     @staticmethod 
     def getCount(baskets, local_freq_itemsets):
-        _list = []
-        _count = 0
+        list = []
+        count = 0
         for _basket in baskets: 
             for _itemset in local_freq_itemsets.value:
-                _count += 1 
-                #print('_count=%d, str(_itemset[0]).split(SPLITTER)=%s, _basket.split(SPLITTER)=%s,set(str(_itemset[0]).split(SPLITTER)).issubset(_basket.split(SPLITTER))=%r'%(_count,str(_itemset[0]).split(SPLITTER), _basket.split(SPLITTER), set(str(_itemset[0]).split(SPLITTER)).issubset(_basket.split(SPLITTER))))
+                count += 1 
+                #print('count=%d, str(_itemset[0]).split(SPLITTER)=%s, _basket.split(SPLITTER)=%s,set(str(_itemset[0]).split(SPLITTER)).issubset(_basket.split(SPLITTER))=%r'%(count,str(_itemset[0]).split(SPLITTER), _basket.split(SPLITTER), set(str(_itemset[0]).split(SPLITTER)).issubset(_basket.split(SPLITTER))))
                 if set(str(_itemset[0]).split(SPLITTER)).issubset(_basket.split(SPLITTER)) :
-                    _list.append((_itemset[0], 1))      
-        if DEBUG : print('str(_list))=%s'%(str(_list)))
-        return iter(_list)
+                    list.append((_itemset[0], 1))      
+        if DEBUG : print('str(list))=%s'%(str(list)))
+        return iter(list)
         
         
     
